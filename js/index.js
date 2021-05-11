6300ca08bbcd77f6512f4a3a9289092f690a171a
IvanOmelchenkoIP <io25112002@gmail.com>
2021-05-11 17:08:24 +0300

    Updated File Reading System and Code Structure a bit

--- js/index.js
+++ js/index.js
@@ -1,38 +1,22 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
+const getStruct = require('./src/extractData.js');
 const fileName = '';
-
 let struct = null;
-const fileTypes = {
-	json: data => {
-		return data;
-	}
-}
 
 fs.readFile(fileName, 'utf-8',(err, data) => {
 	if (err) {
 		console.error(err);
 	} else {
-		struct = getFormat(data, fileName);
+		const format = getDataFormat(fileName);
+		struct = getStruct(data, format);
 	}	
 });
 
-const getFormat = (data, name) => {
-	let obj = null;
-	const msg = 'The structure could not be formed. Wrong file format.'
-	const rexp = /[.][a-z]+/;
-	let [format] = name.match(rexp);
-	format = format.slice(1, format.length);
-	
-	const types = Object.keys(fileTypes);	
-	for (let type of types) {
-		if (type == format) {
-			obj = fileTypes[type](data);
-			break;
-		}
-	}
-
-	if (obj !== null) return obj;
-	else return msg;
+const getDataFormat = name => {
+	let format = path.extname(fileName);
+	format = format.toString().slice(1, format.length);
+	return format;
 }
